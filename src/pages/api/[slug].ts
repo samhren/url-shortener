27407850bed2ae010f2a037dd8170afabbebe6da
SI-86 <samaldenhren@gmail.com>
2022-08-02 00:08:19 -0400

    /api/slug working

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../server/db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const slug = req.query["slug"];
+
+    if (!slug || typeof slug !== "string") {
+        res.statusCode = 404;
+
+        res.send(JSON.stringify({ message: "Please use a slug" }));
+
+        return;
+    }
+
+    const data = await prisma.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug,
+            },
+        },
+    });
+
+    if (!data) {
+        res.statusCode = 404;
+
+        res.send(JSON.stringify({ message: "Slug not found" }));
+
+        return;
+    }
+
+    return res.redirect(data.url);
+};
